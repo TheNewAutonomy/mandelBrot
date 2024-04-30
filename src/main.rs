4beee630dc9a287a109c1ca1d833a2c9063bdc8d
Darren Oliveiro-Priestnall <darren@ourfamily.io>
2024-04-30 20:24:14 +0100

    break into several hundred tasks spread over threads

--- src/main.rs
+++ src/main.rs
@@ -107,6 +107,28 @@ fn main() {
                 .expect("error parsing upper left corner point");
             let lower_right = parse_complex(&args[4])
                 .expect("error parsing lower right corner point");
+
+
+            let mut pixels = vec![0; bounds.0 * bounds.1];
+            {
+                let bands: Vec<(usize, &mut [u8])> = pixels
+                                .chunks_mut(bounds.0)
+                                .enumerate()
+                                .collect();
+
+                            bands.into_iter()
+                                .for_each(|(i,band)| {
+                                    let top = i;
+                                    let band_bounds = (bounds.0,1);
+                                    let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                                    let band_lower_right = pixel_to_point(bounds, (bounds.0, top + 1), upper_left, lower_right);
+                                    render(band, band_bounds, band_upper_left, band_lower_right);
+                                });
+            }
+            write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+            /*
+
+
             let mut pixels = vec![0; bounds.0 * bounds.1];
 
             // render(&mut pixels, bounds, upper_left,lower_right);
@@ -131,10 +153,13 @@ fn main() {
                             }
                                 }).unwrap();
             }
-            //
-            
             write_image(&args[1], &pixels, bounds)
                 .expect("error writing PNG file");
+            */
+
+            //
+            
+            
 }
 
 #[test]
